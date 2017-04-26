2fa88cf99b32534bbcdece35d2de3500be6237a8
cowboydaniel <nskirkey@gmail.com>
2017-04-26 18:02:16 -0400

    feed-database

--- server/src/server.js
+++ server/src/server.js
@@ -18,33 +18,112 @@ var mongo_express = require('mongo-express/lib/middleware');
 // Import the default Mongo Express configuration
 var mongo_express_config = require('mongo-express/config.default.js');
 
-app.use('/mongo_express', mongo_express(mongo_express_config));
+var MongoDB = require('mongodb');
+var MongoClient = MongoDB.MongoClient;
+var ObjectID = MongoDB.ObjectID;
+var url = 'mongodb://localhost:27017/facebook';
 
-app.use(bodyParser.text());
-app.use(bodyParser.json());
-app.use(express.static('../client/build'));
+MongoClient.connect(url, function(err, db) {
+  app.use('/mongo_express', mongo_express(mongo_express_config));
 
+  app.use(bodyParser.text());
+  app.use(bodyParser.json());
+  app.use(express.static('../client/build'));
+/*
+
+  function getFeedItemSync(feedItemId) {
+  var feedItem = readDocument('feedItems', feedItemId);
+  db.collection('helloworld').findOne(query, function(err, doc) {
+    if (err) {
+      // Something bad happened.
+      throw err;
+    } else {
+      // Success! If we found the document, then doc contains
+      // the document. If we did not find the document, doc is
+      // null.
+      callback(doc);
+    }
+  });
+*/
 /**
  * Resolves a feed item. Internal to the server, since it's synchronous.
+ * @param feedItemId The feed item's ID. Must be an ObjectID.
+ * @param callback Called when the operation finishes. First argument is an error object,
+ *   which is null if the operation succeeds, and the second argument is the
+ *   resolved feed item.
  */
-function getFeedItemSync(feedItemId) {
-  var feedItem = readDocument('feedItems', feedItemId);
-  // Resolve 'like' counter.
-  feedItem.likeCounter = feedItem.likeCounter.map((id) => readDocument('users', id));
-  // Assuming a StatusUpdate. If we had other types of FeedItems in the DB, we would
-  // need to check the type and have logic for each type.
-  feedItem.contents.author = readDocument('users', feedItem.contents.author);
-  // Resolve comment author.
-  feedItem.comments.forEach((comment) => {
-    comment.author = readDocument('users', comment.author);
+function getFeedItem(feedItemId, callback) {
+  // Get the feed item with the given ID.
+  db.collection('feedItems').findOne({
+    _id: feedItemId
+  }, function(err, feedItem) {
+    if (err) {
+      // An error occurred.
+      return callback(err);
+    } else if (feedItem === null) {
+      // Feed item not found!
+      return callback(null, null);
+    }
+
+    // Build a list of all of the user objects we need to resolve.
+    // Start off with the author of the feedItem.
+    var userList = [feedItem.contents.author];
+    // Add all of the user IDs in the likeCounter.
+    userList = userList.concat(feedItem.likeCounter);
+    // Add all of the authors of the comments.
+    feedItem.comments.forEach((comment) => userList.push(comment.author));
+    // Resolve all of the user objects!
+    resolveUserObjects(userList, function(err, userMap) {
+      if (err) {
+        return callback(err);
+      }
+      // Use the userMap to look up the author's user object
+      feedItem.contents.author = userMap[feedItem.contents.author];
+      // Look up the user objects for all users in the like counter.
+      feedItem.likeCounter = feedItem.likeCounter.map((userId) => userMap[userId]);
+      // Look up each comment's author's user object.
+      feedItem.comments.forEach((comment) => {
+        comment.author = userMap[comment.author];
+      });
+      // Return the resolved feedItem!
+      callback(null, feedItem);
+    });
   });
-  return feedItem;
+}
+  /**
+ * Resolves a list of user objects. Returns an object that maps user IDs to
+ * user objects.
+ */
+function resolveUserObjects(userList, callback) {
+  // Special case: userList is empty.
+  // It would be invalid to query the database with a logical OR
+  // query with an empty array.
+  if (userList.length === 0) {
+    callback(null, {});
+  } else {
+    // Build up a MongoDB "OR" query to resolve all of the user objects
+    // in the userList.
+    var query = {
+      $or: userList.map((id) => { return {_id: id } })
+    };
+    // Resolve 'like' counter
+    db.collection('users').find(query).toArray(function(err, users) {
+      if (err) {
+        return callback(err);
+      }
+      // Build a map from ID to user object.
+      // (so userMap["4"] will give the user with ID 4)
+      var userMap = {};
+      users.forEach((user) => {
+        userMap[user._id] = user;
+      });
+      callback(null, userMap);
+    });
+  }
 }
 
-/**
- * Get the feed data for a particular user.
- */
-function getFeedData(user) {
+/*
+  function getFeedData(user) {
   var userData = readDocument('users', user);
   var feedData = readDocument('feeds', userData.feed);
   // While map takes a callback, it is synchronous, not asynchronous.
@@ -52,12 +131,79 @@ function getFeedData(user) {
   feedData.contents = feedData.contents.map(getFeedItemSync);
   // Return FeedData with resolved references.
   return feedData;
-}
+  
+*/
 
 /**
- * Get the user ID from a token. Returns -1 (an invalid ID) if it fails.
+ * Get the feed data for a particular user.
+ * @param user The ObjectID of the user document.
  */
-function getUserIdFromToken(authorizationLine) {
+
+function getFeedData(user, callback) {
+  db.collection('users').findOne({
+    _id: user
+  }, function(err, userData) {
+    if (err) {
+      return callback(err);
+    } else if (userData === null) {
+      // User not found.
+      return callback(null, null);
+    }
+
+    db.collection('feeds').findOne({
+      _id: userData.feed
+    }, function(err, feedData) {
+      if (err) {
+        return callback(err);
+      } else if (feedData === null) {
+        // Feed not found.
+        return callback(null, null);
+      }
+
+      // We will place all of the resolved FeedItems here.
+      // When done, we will put them into the Feed object
+      // and send the Feed to the client.
+      var resolvedContents = [];
+
+      // processNextFeedItem is like an asynchronous for loop:
+      // It performs processing on one feed item, and then triggers
+      // processing the next item once the first one completes.
+      // When all of the feed items are processed, it completes
+      // a final action: Sending the response to the client.
+      function processNextFeedItem(i) {
+        // Asynchronously resolve a feed item.
+        getFeedItem(feedData.contents[i], function(err, feedItem) {
+          if (err) {
+            // Pass an error to the callback.
+            callback(err);
+          } else {
+            // Success!
+            resolvedContents.push(feedItem);
+            if (resolvedContents.length === feedData.contents.length) {
+              // I am the final feed item; all others are resolved.
+              // Pass the resolved feed document back to the callback.
+              feedData.contents = resolvedContents;
+              callback(null, feedData);
+            } else {
+              // Process the next feed item.
+              processNextFeedItem(i + 1);
+            }
+          }
+        });
+      }
+
+      // Special case: Feed is empty.
+      if (feedData.contents.length === 0) {
+        callback(null, feedData);
+      } else {
+        processNextFeedItem(0);
+      }
+    });
+  });
+}
+
+/* OLD CODE:
+  function getUserIdFromToken(authorizationLine) {
   try {
     // Cut off "Bearer " from the header value.
     var token = authorizationLine.slice(7);
@@ -77,12 +223,33 @@ function getUserIdFromToken(authorizationLine) {
     // Return an invalid ID.
     return -1;
   }
+  }
+*/
+
+function getUserIdFromToken(authorizationLine) {
+  try {
+    // Cut off "Bearer " from the header value.
+    var token = authorizationLine.slice(7);
+    // Convert the base64 string to a UTF-8 string.
+    var regularString = new Buffer(token, 'base64').toString('utf8');
+    // Convert the UTF-8 string into a JavaScript object.
+    var tokenObj = JSON.parse(regularString);
+    var id = tokenObj['id'];
+    // Check that id is a number.
+    if (typeof id === 'string') {
+      return id;
+    } else {
+      // Not a number. Return "", an invalid ID.
+      return "";
+    }
+  } catch (e) {
+    // Return an invalid ID.
+    return -1;
+  }
 }
 
-/**
- * Get the feed data for a particular user.
- */
-app.get('/user/:userid/feed', function(req, res) {
+  /* OLD CODE:
+  app.get('/user/:userid/feed', function(req, res) {
   var userid = req.params.userid;
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   // userid is a string. We need it to be a number.
@@ -94,12 +261,40 @@ app.get('/user/:userid/feed', function(req, res) {
     // 403: Unauthorized request.
     res.status(403).end();
   }
-});
+  });
+  */
 
-/**
- * Adds a new status update to the database.
- */
-function postStatusUpdate(user, location, contents, image) {
+  /**
+   * Get the feed data for a particular user.
+   */
+  app.get('/user/:userid/feed', function(req, res) {
+    var userid = req.params.userid;
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+    if (fromUser === userid) {
+      // Convert userid into an ObjectID before passing it to database queries.
+      getFeedData(new ObjectID(userid), function(err, feedData) {
+        if (err) {
+          // A database error happened.
+          // Internal Error: 500.
+          res.status(500).send("Database error: " + err);
+        } else if (feedData === null) {
+          // Couldn't find the feed in the database.
+          res.status(400).send("Could not look up feed for user " + userid);
+        } else {
+          // Send data.
+          res.send(feedData);
+        }
+      });
+    } else {
+      // 403: Unauthorized request.
+      res.status(403).end();
+    }
+  });
+
+  /**
+  * Adds a new status update to the database.
+  */
+  function postStatusUpdate(user, location, contents, image) {
   // If we were implementing this for real on an actual server, we would check
   // that the user ID is correct & matches the authenticated user. But since
   // we're mocking it, we can be less strict.
@@ -136,10 +331,10 @@ function postStatusUpdate(user, location, contents, image) {
 
   // Return the newly-posted object.
   return newStatusUpdate;
-}
+  }
 
-//`POST /feeditem { userId: user, location: location, contents: contents  }`
-app.post('/feeditem', validate({ body: StatusUpdateSchema }), function(req, res) {
+  //`POST /feeditem { userId: user, location: location, contents: contents  }`
+  app.post('/feeditem', validate({ body: StatusUpdateSchema }), function(req, res) {
   // If this function runs, `req.body` passed JSON validation!
   var body = req.body;
   var fromUser = getUserIdFromToken(req.get('Authorization'));
@@ -158,10 +353,10 @@ app.post('/feeditem', validate({ body: StatusUpdateSchema }), function(req, res)
     // 401: Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-// `PUT /feeditem/feedItemId/likelist/userId` content
-app.put('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+  // `PUT /feeditem/feedItemId/likelist/userId` content
+  app.put('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   // Convert params from string to number.
   var feedItemId = parseInt(req.params.feeditemid, 10);
@@ -179,10 +374,10 @@ app.put('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
     // 401: Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-// Unlike a feed item.
-app.delete('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
+  // Unlike a feed item.
+  app.delete('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   // Convert params from string to number.
   var feedItemId = parseInt(req.params.feeditemid, 10);
@@ -201,10 +396,10 @@ app.delete('/feeditem/:feeditemid/likelist/:userid', function(req, res) {
     // 401: Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-// `PUT /feeditem/feedItemId/content newContent`
-app.put('/feeditem/:feeditemid/content', function(req, res) {
+  // `PUT /feeditem/feedItemId/content newContent`
+  app.put('/feeditem/:feeditemid/content', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   var feedItemId = req.params.feeditemid;
   var feedItem = readDocument('feedItems', feedItemId);
@@ -225,10 +420,10 @@ app.put('/feeditem/:feeditemid/content', function(req, res) {
     // 401: Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-// `DELETE /feeditem/:id`
-app.delete('/feeditem/:feeditemid', function(req, res) {
+  // `DELETE /feeditem/:id`
+  app.delete('/feeditem/:feeditemid', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   // Convert from a string into a number.
   var feedItemId = parseInt(req.params.feeditemid, 10);
@@ -255,10 +450,10 @@ app.delete('/feeditem/:feeditemid', function(req, res) {
     // 401: Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-//`POST /search queryText`
-app.post('/search', function(req, res) {
+  //`POST /search queryText`
+  app.post('/search', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   var user = readDocument('users', fromUser);
   if (typeof(req.body) === 'string') {
@@ -284,10 +479,10 @@ app.post('/search', function(req, res) {
     // 400: Bad Request.
     res.status(400).end();
   }
-});
+  });
 
-// Post a comment
-app.post('/feeditem/:feeditemid/comments', validate({ body: CommentSchema }), function(req, res) {
+  // Post a comment
+  app.post('/feeditem/:feeditemid/comments', validate({ body: CommentSchema }), function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   var comment = req.body;
   var author = req.body.author;
@@ -310,9 +505,9 @@ app.post('/feeditem/:feeditemid/comments', validate({ body: CommentSchema }), fu
     // Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-app.put('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', function(req, res) {
+  app.put('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   var userId = parseInt(req.params.userid, 10);
   var feedItemId = parseInt(req.params.feeditemid, 10);
@@ -333,9 +528,9 @@ app.put('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', functio
     // Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-app.delete('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', function(req, res) {
+  app.delete('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', function(req, res) {
   var fromUser = getUserIdFromToken(req.get('Authorization'));
   var userId = parseInt(req.params.userid, 10);
   var feedItemId = parseInt(req.params.feeditemid, 10);
@@ -355,20 +550,20 @@ app.delete('/feeditem/:feeditemid/comments/:commentindex/likelist/:userid', func
     // Unauthorized.
     res.status(401).end();
   }
-});
+  });
 
-// Reset database.
-app.post('/resetdb', function(req, res) {
+  // Reset database.
+  app.post('/resetdb', function(req, res) {
   console.log("Resetting database...");
   // This is a debug route, so don't do any validation.
   database.resetDatabase();
   res.send();
-});
+  });
 
-/**
- * Translate JSON Schema Validation failures into error 400s.
- */
-app.use(function(err, req, res, next) {
+  /**
+  * Translate JSON Schema Validation failures into error 400s.
+  */
+  app.use(function(err, req, res, next) {
   if (err.name === 'JsonSchemaValidation') {
     // Set a bad request http response status
     res.status(400).end();
@@ -376,9 +571,10 @@ app.use(function(err, req, res, next) {
     // It's some other sort of error; pass it to next error middleware handler
     next(err);
   }
-});
+  });
 
-// Starts the server on port 3000!
-app.listen(3000, function () {
+  // Starts the server on port 3000!
+  app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+  });
 });
